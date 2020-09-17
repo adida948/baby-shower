ea26bd79c7d2fafbe74bf37f39deaf4c870b4f43
Danilo Carrion <danilo.x.carrio@aexp.com>
2020-09-17 04:30:45 -0400

    fix(Name): Proper indent and names

--- app/components/pages/Home.js
+++ app/components/pages/Home.js
@@ -4,9 +4,9 @@ const Home =  () =>
 <div>
     <div className="whitesquare">
         <div className=" bgimg w3-display-middle w3-text-black padding-200">
-            <h2>Let's taco about a baby</h2>
-            <h1>Danilo Jr Carrion's Baby shower!</h1>
-            <h2><b>September 19th, 2020</b></h2>
+            <h1>Let's taco about a baby</h1>
+            <h3>Danilo Jr. Carrion's Baby Shower!</h3>
+            <h3><b>September 19th, 2020</b></h3>
         </div>
     </div>
 </div>
